5d4011df11506fba943c78163b4dabe9885f9dc9
zhuanghua <30758919@qq.com>
2016-06-11 02:31:28 +0800

    test

--- src/main/python/chengducloud/wuhouapp.py
+++ src/main/python/chengducloud/wuhouapp.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+@app.route("/test")
+def hellotest():
+    return "Hello World Test !"
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0",debug=True)
